783a29ba43a456c7aa2786c2f6b02faeb260ca4a
yys-hub <861768573@qq.com>
2024-04-15 20:08:58 +0800

    Initial commit

--- commit_code_to_git_repository.sh
+++ commit_code_to_git_repository.sh
@@ -1,5 +1,5 @@
 git init
 git add .
 git commit -m "Initial commit"
-git remote add origin <https://github.com/yys-hub/yys-hub>
+git remote add origin <https://github.com/yys-hub/my_repository.git>
 git push -u origin master
